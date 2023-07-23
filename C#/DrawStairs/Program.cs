97e6498a15e00cfe854e66b09bacae6def0744f5
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-23 18:48:52 -0300

    Added two new exercises.

---
+++ C#/DrawStairs/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine(Kata.DrawStairs(1));
+Console.WriteLine(Kata.DrawStairs(2));
+Console.WriteLine(Kata.DrawStairs(3));
+
+public class Kata
+{
+    public static string DrawStairs(int n)
+    {
+        string stairs = "";
+        for (int i = 0; i < n; i++)
+        {
+            stairs += new string(' ', i) + "I\n";
+        }
+        return stairs.TrimEnd();
+    }
+}
---
+++ C#/RemoveDuplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RemoveDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public static class Kata
+    {
+        public static int[] distinct(int[] a)
+        {
+            HashSet<int> uniquesNums = new HashSet<int>();
+            List<int> result = new List<int>();
+
+            foreach (int number in a)
+            {
+                if (!uniquesNums.Contains(number))
+                {
+                    uniquesNums.Add(number);
+                    result.Add(number);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+
+}
